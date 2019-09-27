a3468dc60743b33ba1c6132bd2411ffb39048f12
Westley Andrew <westleyandrew@hotmail.com>
2019-09-27 11:08:24 +0100

    added components

---
+++ src/components/app.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+import SearchBar from './search_bar.jsx';
+import Gif from './gif.jsx';
+import GifList from './gif_list.jsx';
+
+class App extends Component {
+  render () {
+    const gifs = [
+      { id: "xT9IgDEI1iZyb2wqo8"},
+      { id: "3ohxdIrmkBViO8uouc"}
+    ];
+    return (
+      <div>
+        <div className="left-scene">
+          <SearchBar />
+          <div className="selected-gif">
+          <Gif id="xT9IgDEI1iZyb2wqo8" />
+          </div>
+        </div>
+        <div className="right-scene">
+        <GifList gifs={gifs}/>
+        </div>
+      </div>
+      );
+  }
+}
+
+export default App;
